821910848104d14487234d333eeae2e7e5a0c014
NB000139\OPS-USER <zhaira.pacheco@opsolutions.biz>
2025-03-05 16:42:46 +0800

    Added Services and Multiple Add

--- model/customerModel.js
+++ model/customerModel.js
@@ -4,6 +4,7 @@ const CustomerSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true },
+    address: { type: String, required: true },
 });
 
 const Customer = mongoose.model("Customer", CustomerSchema);
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getAllCustomer, getCustomerById, addCustomer, updateCustomer, deleteCustomer } = require("../controllers/customerController");
+const { getAllCustomer, getCustomerById, addCustomer, addAllCustomer, updateCustomer, deleteCustomer } = require("../controller/customerController");
 
 const router = express.Router();
 
 router.get("/", getAllCustomer);
 router.get("/:id", getCustomerById);
 router.post("/", addCustomer);
+router.post("/list", addAllCustomer);
 router.put("/:id", updateCustomer);
 router.delete("/:id", deleteCustomer);
 
